2b2d9bd603793a8efad26f044bbad55dbf5bd507
brunooassuncao <bas@cesar.school>
2025-07-25 15:50:26 -0300

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
-  validates :content, presence: true
+  validates :title, presence: true
   validates :title, length: { minimum: 5 }
+
+  before_save do
+        self.title = self.title + " bruno"
+  end
 end
